f800b106c234a1d4520e468cf5729b6cb1a0b2a2
LucaDRF <lucadr.fiorenzano@gmail.com>
2025-02-24 11:25:40 -0300

    implemented occupied and availble chairs on chair status component

--- src/app/home/chairs/chair-status/chair-status.component.ts
+++ src/app/home/chairs/chair-status/chair-status.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Chair } from '../../../interfaces/chair.interface';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-chair-status',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './chair-status.component.html',
   styleUrl: './chair-status.component.css'
 })
 export class ChairStatusComponent {
-
+  @Input({required: true}) chair: Chair = {} as Chair;
 }
