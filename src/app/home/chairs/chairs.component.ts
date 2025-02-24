914005a9f733488926444d4fda9ccbd9cc22b3ea
LucaDRF <lucadr.fiorenzano@gmail.com>
2025-02-24 11:26:00 -0300

    added loading and integration with api

--- src/app/home/chairs/chairs.component.ts
+++ src/app/home/chairs/chairs.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { UserAssignmentComponent } from './user-assignment/user-assignment.component';
+import { ChairStatusComponent } from "./chair-status/chair-status.component";
+import { CommonModule } from '@angular/common';
+import { Chair, Chairs } from '../../interfaces/chair.interface';
+import { ChairsService } from '../../services/chairs.service';
+import { ChairDetailedComponent } from './chair-detailed/chair-detailed.component';
 
 @Component({
   selector: 'app-chairs',
   standalone: true,
-  imports: [UserAssignmentComponent],
+  imports: [UserAssignmentComponent, ChairStatusComponent, ChairDetailedComponent, CommonModule],
   templateUrl: './chairs.component.html',
   styleUrl: './chairs.component.css'
 })
-export class ChairsComponent {
+export class ChairsComponent implements OnInit {
+  private chairsService = inject(ChairsService);
 
+  public chairsByLine: Chairs[] = [];
+  public selectedChair: any;
+  public chairs: Chairs = [];
+  public loading = false;
+  public actionsLoading = { loading: false };
+
+  async ngOnInit() {
+    this.loading = true;
+    this.chairs = await this.chairsService.getChairsStatus();
+    this.loading = false;
+
+    this.verifyEachChair();
+  }
+
+  verifyEachChair() {
+    this.chairs.forEach((chair: Chair) => {
+      if (!this.chairsByLine[chair.line_value]) {
+        this.chairsByLine[chair.line_value] = [];
+      }
+
+      this.chairsByLine[chair.line_value][chair.column_value] = chair;
+    });
+  }
 }
