9cc4a3221e06aea56009fa25c9a101060b2dd490
LucaDRF <lucadr.fiorenzano@gmail.com>
2025-02-24 02:48:58 -0300

    added chair status component

---
+++ src/app/home/chairs/chair-status/chair-status.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chair-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './chair-status.component.html',
+  styleUrl: './chair-status.component.css'
+})
+export class ChairStatusComponent {
+
+}
